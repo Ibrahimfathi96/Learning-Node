889c7cd2561b402f732b6c14876e52adf777a093
Ibrahim Fathi <himafathy796@gmail.comhimafathy796@gmail.com>
2023-09-16 14:51:22 +0300

    Writeable / Readable Streams

--- index.js
+++ index.js
@@ -1,44 +1,10 @@
 const fs = require("node:fs");
 
-//read file
-// console.log("first");
-// const fileContent = fs.readFileSync("./test.text", "utf-8");//Blocking Code, blocks here until file is read
-// console.log("second");
-//  console.log(`fileContent: ${fileContent}`);
-
-
-//Non Blocking Code
-fs.readFile("./test.text", "utf-8", (err, data) => {//without 'utf-8' buffer will be the output.
-  
-  err
-    ? console.log("Error Raading Files", err)
-    : console.log("File Content ==>", data);
+//Streams [readable - writeable]
+const rStream = fs.createReadStream("./test.txt", "utf-8");
+const wStream = fs.createWriteStream("./readStreamOutput.txt", "utf-8");
+rStream.on("data", (chunk) => {
+  console.log("==================Chunk================", chunk);
+  wStream.write("\n==================Chunk================\n");
+  wStream.write(chunk);
 });
-
-//write to file
-// fs.writeFile(
-//   "./users.json",
-//   JSON.stringify([
-//     {
-//       id: 1,
-//       name: "Ibrahim",
-//     },
-//     {
-//       id: 2,
-//       name: "Mostafa",
-//     },
-//     {
-//       id: 3,
-//       name: "Dayhanna",
-//     },
-//   ]),
-//   "utf-8",
-//   (err) => {
-//     err ? console.log("Error Writing The file", err) : console.log("Done");
-//   }
-// );
-
-//delete file
-// fs.unlink("./testdel.txt", (err) => {
-//   err ? console.log("Error Deleting the file", err) : console.log("Done");
-// });
