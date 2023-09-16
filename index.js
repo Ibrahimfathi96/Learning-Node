9c8c64fe184c1ef2161014e7c7605a323408407a
Ibrahim Fathi <himafathy796@gmail.comhimafathy796@gmail.com>
2023-09-16 14:40:29 +0300

    read / write / delete files from fileSystems

--- index.js
+++ index.js
@@ -6,37 +6,39 @@ const fs = require("node:fs");
 // console.log("second");
 //  console.log(`fileContent: ${fileContent}`);
 
-fs.readFile("./test.text", "utf-8", (err, data) => {
-  //Non Blocking Code
+
+//Non Blocking Code
+fs.readFile("./test.text", "utf-8", (err, data) => {//without 'utf-8' buffer will be the output.
+  
   err
     ? console.log("Error Raading Files", err)
     : console.log("File Content ==>", data);
 });
 
 //write to file
-fs.writeFile(
-  "./users.json",
-  JSON.stringify([
-    {
-      id: 1,
-      name: "Ibrahim",
-    },
-    {
-      id: 2,
-      name: "Mostafa",
-    },
-    {
-      id: 3,
-      name: "Dayhanna",
-    },
-  ]),
-  "utf-8",
-  (err) => {
-    err ? console.log("Error Writing The file", err) : console.log("Done");
-  }
-);
+// fs.writeFile(
+//   "./users.json",
+//   JSON.stringify([
+//     {
+//       id: 1,
+//       name: "Ibrahim",
+//     },
+//     {
+//       id: 2,
+//       name: "Mostafa",
+//     },
+//     {
+//       id: 3,
+//       name: "Dayhanna",
+//     },
+//   ]),
+//   "utf-8",
+//   (err) => {
+//     err ? console.log("Error Writing The file", err) : console.log("Done");
+//   }
+// );
 
 //delete file
-fs.unlink("./testdel.txt", (err) => {
-  err ? console.log("Error Deleting the file", err) : console.log("Done");
-});
+// fs.unlink("./testdel.txt", (err) => {
+//   err ? console.log("Error Deleting the file", err) : console.log("Done");
+// });
