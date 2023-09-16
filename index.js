cbda27b503d4999d0b2036d58bfd28c41b02d82f
Ibrahim Fathi <himafathy796@gmail.comhimafathy796@gmail.com>
2023-09-16 14:21:25 +0300

    read / write / delete files from fileSystems

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+const fs = require("node:fs");
 
+//read file
+// console.log("first");
+// const fileContent = fs.readFileSync("./test.text", "utf-8");//Blocking Code, blocks here until file is read
+// console.log("second");
+//  console.log(`fileContent: ${fileContent}`);
+
+fs.readFile("./test.text", "utf-8", (err, data) => {
+  //Non Blocking Code
+  err
+    ? console.log("Error Raading Files", err)
+    : console.log("File Content ==>", data);
+});
+
+//write to file
+fs.writeFile(
+  "./users.json",
+  JSON.stringify([
+    {
+      id: 1,
+      name: "Ibrahim",
+    },
+    {
+      id: 2,
+      name: "Mostafa",
+    },
+    {
+      id: 3,
+      name: "Dayhanna",
+    },
+  ]),
+  "utf-8",
+  (err) => {
+    err ? console.log("Error Writing The file", err) : console.log("Done");
+  }
+);
+
+//delete file
+fs.unlink("./testdel.txt", (err) => {
+  err ? console.log("Error Deleting the file", err) : console.log("Done");
+});
