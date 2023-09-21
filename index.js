9bd27eec9ce977207066ce94496ecd4735af72ab
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2023-09-21 12:44:24 -0700

    end of Session 2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hola Bola aaaa")
+// import fs from "node:fs";
+const fs = require("node:fs");
+console.log("First");
+// console.log("FileContent", fs.readFileSync("./test.txt", "utf-8"));
+
+fs.readFile("./test.txt", "utf-8", (err, content) => {
+  err
+    ? console.error("Error Reading the file ==>", err)
+    : console.log("fileContent:", content);
+});//libvu ==> File I/O ==> Thread Pool ==> async operations / functions
+
+console.log("Second");
