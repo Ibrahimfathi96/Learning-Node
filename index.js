b75a946a66b206ebc15a58272b241f5158ca402e
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2023-09-21 23:23:51 +0300

    network request

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
-// import fs from "node:fs";
-const crypto = require("node:crypto");
-process.env.UV_THREADPOOL_SIZE = 5;
+// // import fs from "node:fs";
+// const crypto = require("node:crypto");
+// process.env.UV_THREADPOOL_SIZE = 5;
 const start = performance.now();
 
-crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
-  console.log("End of PBKDF2 in ms", performance.now() - start);
+// crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
+//   console.log("End of PBKDF2 in ms", performance.now() - start);
+// });
+
+// crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
+//   console.log("End of PBKDF2 in ms", performance.now() - start);
+// });
+
+// crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
+//   console.log("End of PBKDF2 in ms", performance.now() - start);
+// });
+
+// crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
+//   console.log("End of PBKDF2 in ms", performance.now() - start);
+// });
+
+// crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
+//   //No.5 will have larger time because the default size is 4 only but there's a sol. process.env.UV_THREADPOOL_SIZE = 5;
+//   console.log("End of PBKDF2 in ms", performance.now() - start);
+// });
+
+// // crypto.pbkdf2Sync("secret", "salt", 100000, 64, "sha512");
+// // console.log("End of PBKDF2 in ms", performance.now() - start);
+
+// // crypto.pbkdf2Sync("secret", "salt", 100000, 64, "sha512");
+// // console.log("End of PBKDF2 in ms", performance.now() - start);
+fetch("https://dummyjson.com/products").then(() => {
+  console.log("End of Request in ms", performance.now() - start);
+});
+fetch("https://dummyjson.com/products").then(() => {
+  console.log("End of Request in ms", performance.now() - start);
+});
+fetch("https://dummyjson.com/products").then(() => {
+  console.log("End of Request in ms", performance.now() - start);
+});
+fetch("https://dummyjson.com/products").then(() => {
+  console.log("End of Request in ms", performance.now() - start);
 });
 
-crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
-  console.log("End of PBKDF2 in ms", performance.now() - start);
-});
 
-crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
-  console.log("End of PBKDF2 in ms", performance.now() - start);
-});
-
-crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
-  console.log("End of PBKDF2 in ms", performance.now() - start);
-});
-
-crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
-  //No.5 will have larger time because the default size is 4 only but there's a sol. process.env.UV_THREADPOOL_SIZE = 5;
-  console.log("End of PBKDF2 in ms", performance.now() - start);
-});
-
-// crypto.pbkdf2Sync("secret", "salt", 100000, 64, "sha512");
-// console.log("End of PBKDF2 in ms", performance.now() - start);
-
-// crypto.pbkdf2Sync("secret", "salt", 100000, 64, "sha512");
-// console.log("End of PBKDF2 in ms", performance.now() - start);
