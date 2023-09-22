8198ce7e5919ca2a8c306f2b000884a05fe478f3
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2023-09-22 22:39:57 +0300

    CRUD Operations with Mongoose

--- Session4-SimpleAPI/controllers/courses.controller.js
+++ Session4-SimpleAPI/controllers/courses.controller.js
@@ -1,48 +1,62 @@
-let { courses } = require("../data/courses");
 const { validationResult } = require("express-validator");
+const Course = require("../models/course.model");
+
+// //LOCAL WITHOUT DB
+// let { courses } = require("../data/courses");
 
 //Route Handlers
-const getAllCourses = (req, res) => {
+const getAllCourses = async (req, res) => {
+  //getAllCourses from DB using Course model
+  const courses = await Course.find();
+  console.log("Courses", courses);
   res.json(courses);
 };
 
-const getSingleCourse = (req, res) => {
-  const courseId = +req.params.courseId;
-  const course = courses.find(course => course.id === courseId);
-  if (!course) {
-    return res.status(404).json({ code: 404, msg: "course not found!" });
+const getSingleCourse = async (req, res) => {
+  try {
+    const course = await Course.findById(req.params.courseId);
+    if (!course) {
+      return res.status(404).json({ code: 404, msg: "Course not found!" });
+    }
+    res.json(course);
+  } catch (err) {
+    return res.status(400).json({ code: 400, msg: "Invalid Object ID!" });
   }
-  res.json(course);
 };
 
-const addNewCourse = (req, res) => {
+const addNewCourse = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
 
-  const course = {
-    id: courses.length + 1,
-    ...req.body
-  };
-  courses.push(course);
-  res.status(201).json(course);
+  const newCourse = new Course(req.body);
+  await newCourse.save();
+  res.status(201).json(newCourse);
 };
 
-const updateCourse = (req, res) => {
-  const id = +req.params.courseId;
-  let course = courses.find(course => course.id === id);
-  if (!course) {
-    return res.status(404).json({ code: 404, msg: "course not found!" });
+const updateCourse = async (req, res) => {
+  const courseId = req.params.courseId;
+  try {
+    const updatedCourse = await Course.updateOne(
+      { _id: courseId },
+      {
+        $set: { ...req.body }
+      }
+    );
+    res.status(200).json(updatedCourse);
+  } catch (err) {
+    return res.status(400).json({ errorMsg: err });
   }
-  course = { ...course, ...req.body };
-  res.status(200).json(course);
 };
 
-const deleteCourse = (req, res) => {
-  const id = +req.params.courseId;
-  let coursesList = courses.filter(course => course.id != id);
-  res.status(200).json([{ success: "true" }, coursesList]);
+const deleteCourse = async (req, res) => {
+  try {
+    const data = await Course.deleteOne({ _id: req.params.courseId });
+    res.status(200).json({ success: true, msg: data });
+  } catch (err) {
+    return res.status(400).json({ errorMsg: err });
+  }
 };
 
 module.exports = {
--- Session4-SimpleAPI/index.js
+++ Session4-SimpleAPI/index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const url =
+  "mongodb+srv://ibmf796:nodejs_123@learn-mongo-db.p2rnxjy.mongodb.net/IBMF?retryWrites=true&w=majority";
+mongoose.connect(url).then(() => {
+  console.log("mongodb server started successfully");
+});
 app.use(express.json());
 const port = 4000;
 
